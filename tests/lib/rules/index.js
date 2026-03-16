1d6e53ceae073693c7f3560737b0fd26e5ba49ec
Darshan Jain <djainj86@gmail.com>
2026-03-16 19:32:08 -0400

    test: make importKind RuleTester cases compatible across eslint versions

--- tests/lib/rules/index.js
+++ tests/lib/rules/index.js
@@ -2,53 +2,39 @@ const RuleTester = require("eslint").RuleTester;
 const rule = require("../../../lib/rules/no-global");
 const eslintMajor = Number(require("eslint/package.json").version.split(".")[0]);
 let typescriptParser;
+let typescriptParserPath;
 
 try {
   typescriptParser = require("@typescript-eslint/parser");
+  typescriptParserPath = require.resolve("@typescript-eslint/parser");
 } catch {
   typescriptParser = null;
+  typescriptParserPath = null;
 }
 const canRunImportKindCases = Boolean(typescriptParser) && eslintMajor < 10;
 
 const ruleTester = new RuleTester(
   eslintMajor >= 9
-    ? { languageOptions: { ecmaVersion: 2020, sourceType: "module" } }
-    : { parserOptions: { ecmaVersion: 2020, sourceType: "module" } }
+    ? { languageOptions: { ecmaVersion: 2022, sourceType: "module" } }
+    : { parserOptions: { ecmaVersion: 2022, sourceType: "module" } }
 );
 
 const parserSpecificValidCases = canRunImportKindCases
   ? [
       eslintMajor >= 9
         ? {
-            code: "import type _ from 'lodash';",
-            languageOptions: {
-              ecmaVersion: 2020,
-              sourceType: "module",
-              parser: typescriptParser,
-            },
-          }
-        : {
-            code: "import type _ from 'lodash';",
-            parser: "@typescript-eslint/parser",
-            parserOptions: {
-              ecmaVersion: 2020,
-              sourceType: "module",
-            },
-          },
-      eslintMajor >= 9
-        ? {
             code: "import type { debounce } from 'lodash';",
             languageOptions: {
-              ecmaVersion: 2020,
+              ecmaVersion: 2022,
               sourceType: "module",
               parser: typescriptParser,
             },
           }
         : {
             code: "import type { debounce } from 'lodash';",
-            parser: "@typescript-eslint/parser",
+            parser: typescriptParserPath,
             parserOptions: {
-              ecmaVersion: 2020,
+              ecmaVersion: 2022,
               sourceType: "module",
             },
           },
@@ -56,16 +42,16 @@ const parserSpecificValidCases = canRunImportKindCases
         ? {
             code: "import { type debounce } from 'lodash';",
             languageOptions: {
-              ecmaVersion: 2020,
+              ecmaVersion: 2022,
               sourceType: "module",
               parser: typescriptParser,
             },
           }
         : {
             code: "import { type debounce } from 'lodash';",
-            parser: "@typescript-eslint/parser",
+            parser: typescriptParserPath,
             parserOptions: {
-              ecmaVersion: 2020,
+              ecmaVersion: 2022,
               sourceType: "module",
             },
           },
@@ -76,9 +62,30 @@ const parserSpecificInvalidCases = canRunImportKindCases
   ? [
       eslintMajor >= 9
         ? {
+            code: "import type _ from 'lodash';",
+            languageOptions: {
+              ecmaVersion: 2022,
+              sourceType: "module",
+              parser: typescriptParser,
+            },
+            errors: [{ messageId: "invalidDefaultImport" }],
+            output: null,
+          }
+        : {
+            code: "import type _ from 'lodash';",
+            parser: typescriptParserPath,
+            parserOptions: {
+              ecmaVersion: 2022,
+              sourceType: "module",
+            },
+            errors: [{ messageId: "invalidDefaultImport" }],
+            output: null,
+          },
+      eslintMajor >= 9
+        ? {
             code: "import { debounce, type isEqual } from 'lodash';",
             languageOptions: {
-              ecmaVersion: 2020,
+              ecmaVersion: 2022,
               sourceType: "module",
               parser: typescriptParser,
             },
@@ -87,9 +94,9 @@ const parserSpecificInvalidCases = canRunImportKindCases
           }
         : {
             code: "import { debounce, type isEqual } from 'lodash';",
-            parser: "@typescript-eslint/parser",
+            parser: typescriptParserPath,
             parserOptions: {
-              ecmaVersion: 2020,
+              ecmaVersion: 2022,
               sourceType: "module",
             },
             errors: [{ messageId: "invalidImport" }],
