d413d8c05b9fbb29daddc123141835f7439174b1
Darshan Jain <djainj86@gmail.com>
2026-03-16 23:34:06 -0400

    Add importKind RuleTester coverage for no-global rule (#15)
    This expands no-global rule coverage with RuleTester cases for TypeScript importKind syntax and updates the test setup so those cases run consistently across supported ESLint versions.

--- tests/lib/rules/index.js
+++ tests/lib/rules/index.js
@@ -1,17 +1,115 @@
 const RuleTester = require("eslint").RuleTester;
 const rule = require("../../../lib/rules/no-global");
 const eslintMajor = Number(require("eslint/package.json").version.split(".")[0]);
+let typescriptParser;
+let typescriptParserPath;
+
+try {
+  typescriptParser = require("@typescript-eslint/parser");
+  typescriptParserPath = require.resolve("@typescript-eslint/parser");
+} catch {
+  typescriptParser = null;
+  typescriptParserPath = null;
+}
+const canRunImportKindCases = Boolean(typescriptParser) && eslintMajor < 10;
 
 const ruleTester = new RuleTester(
   eslintMajor >= 9
-    ? { languageOptions: { ecmaVersion: 2020, sourceType: "module" } }
-    : { parserOptions: { ecmaVersion: 2020, sourceType: "module" } }
+    ? { languageOptions: { ecmaVersion: 2022, sourceType: "module" } }
+    : { parserOptions: { ecmaVersion: 2022, sourceType: "module" } }
 );
+
+const parserSpecificValidCases = canRunImportKindCases
+  ? [
+      eslintMajor >= 9
+        ? {
+            code: "import type { debounce } from 'lodash';",
+            languageOptions: {
+              ecmaVersion: 2022,
+              sourceType: "module",
+              parser: typescriptParser,
+            },
+          }
+        : {
+            code: "import type { debounce } from 'lodash';",
+            parser: typescriptParserPath,
+            parserOptions: {
+              ecmaVersion: 2022,
+              sourceType: "module",
+            },
+          },
+      eslintMajor >= 9
+        ? {
+            code: "import { type debounce } from 'lodash';",
+            languageOptions: {
+              ecmaVersion: 2022,
+              sourceType: "module",
+              parser: typescriptParser,
+            },
+          }
+        : {
+            code: "import { type debounce } from 'lodash';",
+            parser: typescriptParserPath,
+            parserOptions: {
+              ecmaVersion: 2022,
+              sourceType: "module",
+            },
+          },
+    ]
+  : [];
+
+const parserSpecificInvalidCases = canRunImportKindCases
+  ? [
+      eslintMajor >= 9
+        ? {
+            code: "import type _ from 'lodash';",
+            languageOptions: {
+              ecmaVersion: 2022,
+              sourceType: "module",
+              parser: typescriptParser,
+            },
+            errors: [{ messageId: "invalidDefaultImport" }],
+            output: null,
+          }
+        : {
+            code: "import type _ from 'lodash';",
+            parser: typescriptParserPath,
+            parserOptions: {
+              ecmaVersion: 2022,
+              sourceType: "module",
+            },
+            errors: [{ messageId: "invalidDefaultImport" }],
+            output: null,
+          },
+      eslintMajor >= 9
+        ? {
+            code: "import { debounce, type isEqual } from 'lodash';",
+            languageOptions: {
+              ecmaVersion: 2022,
+              sourceType: "module",
+              parser: typescriptParser,
+            },
+            errors: [{ messageId: "invalidImport" }],
+            output: "import debounce from 'lodash/debounce';",
+          }
+        : {
+            code: "import { debounce, type isEqual } from 'lodash';",
+            parser: typescriptParserPath,
+            parserOptions: {
+              ecmaVersion: 2022,
+              sourceType: "module",
+            },
+            errors: [{ messageId: "invalidImport" }],
+            output: "import debounce from 'lodash/debounce';",
+          },
+    ]
+  : [];
 
 ruleTester.run("lodash-specific-import/no-global", rule, {
   valid: [
     "import map from 'lodash/map';",
     "import map from 'lodash-es/map';",
+    ...parserSpecificValidCases,
   ],
 
   invalid: [
@@ -67,5 +165,6 @@ ruleTester.run("lodash-specific-import/no-global", rule, {
       errors: [{ messageId: "invalidImport" }],
       output: null,
     },
+    ...parserSpecificInvalidCases,
   ],
 });
